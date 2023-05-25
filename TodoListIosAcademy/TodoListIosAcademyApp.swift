ee5327b38125b9585c41cd2f1fdbfaca2eff996c
ducanh261101a <thaianh02012002@gmail.com>
2023-05-25 16:29:20 +0700

    Initial Commit

---
+++ TodoListIosAcademy/TodoListIosAcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListIosAcademyApp.swift
+//  TodoListIosAcademy
+//
+//  Created by Duc Anh on 25/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListIosAcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
